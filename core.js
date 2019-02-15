5c9e0ebdc56c5b07a7fc07c413f4c34a25e5b226
Jarrod Davis <developer@jarrodldavis.com>
2019-02-14 23:44:58 -0700

    Use node.js instead of bash
    Bash doesn't like trying to source relative scripts.
    
    Use node.js instead so I can keep all of the logic unduplicated (plus
    JavaScript is easier to read than bash).

---
+++ core.js
@@ -0,0 +1,110 @@
+const { execSync } = require('child_process')
+
+const packageVersion = process.env.npm_package_version;
+const versionPrefix = process.env.npm_config_tag_version_prefix;
+
+function exit(message) {
+  console.error(`npm version: ${message}`);
+  process.exit(1);
+}
+
+if (typeof packageVersion !== 'string' || packageVersion.length === 0) {
+  exit('$npm_package_version must be set');
+}
+
+if (typeof versionPrefix !== 'string' || versionPrefix.length === 0) {
+  exit('$npm_config_tag_version_prefix must be set');
+}
+
+function graphql(query) {
+  const fullQuery = `query { repository(owner:"{owner}", name:"{repo}") { ${query} } }`;
+  const command = `hub api graphql -f query="${fullQuery}"`;
+
+  let result;
+  try {
+    result = JSON.parse(execSync(command));
+  } catch(error) {
+    exit(`failed to execute query: ${error}`)
+  }
+
+  if (typeof result !== 'object') {
+    exit(`unexpected result ${result} from query`);
+  } else if (result.hasOwnProperty('errors')) {
+    exit(`query returned errors: ${JSON.stringify(result.errors)}`);
+  } else if (typeof result.data !== 'object' || typeof result.data.repository !== 'repository') {
+    exit(`could not find repository data from query result`);
+  } else {
+    return result.data.repository;
+  }
+}
+
+function getDefaultBranch() {
+  return graphql("defaultBranchRef { name }").defaultBranchRef.name;
+}
+
+function ensureSynchronized() {
+  const current = execSync('hub rev-parse --abbrev-ref HEAD');
+  const upstream = execSync('hub rev-parse --abbrev-ref --symbolic-full-name @{u}');
+  const range = execSync(`hub rev-parse -q ${current} ${upstream}`);
+  const [start, end] = range.split(/(\s+)/);
+  if (start !== end) {
+    exit('local branch is not in sync with upstream');
+  }
+}
+
+function determineProductionBranch() {
+  const branches = graphql('protectedBranches(first: 3) { nodes { name } }').protectedBranches.nodes;
+
+  if (branches.length !== 2) {
+    exit(`expected two protected branches but found ${branches.length}`);
+  }
+
+  const [{ name: firstBranch }, { name: secondBranch }] = branches;
+
+  const defaultBranch = getDefaultBranch();
+
+  switch (defaultBranch) {
+    case firstBranch:
+      return secondBranch;
+    case secondBranch:
+      return firstBranch;
+    default:
+      return exit('could not determine production branch because neither protected branch mataches the default branch');
+  }
+}
+
+function preversion() {
+  const defaultBranch = getDefaultBranch();
+
+  // checkout development branch
+  execSync(`hub checkout ${defaultBranch}`);
+  // bring in any changes from upstream
+  execSync(`hub sync`);
+  // ensure local and remote head branch are in sync (`hub sync` only warns)
+  ensureSynchronized();
+  // run test suite
+  execSync(`npm test`);
+}
+
+function version() {
+  const bumpPlugin = `@jarrodldavis/changelog-version-bump=version:'${packageVersion}'`;
+
+  // create new release branch
+  execSync(`hub checkout -b release/${packageVersion}`);
+  // update changelog with new version
+  execSync(`remark CHANGELOG.md -o --use ${bumpPlugin}`);
+  // add changelog to staging (npm will handle creating the commit)
+  execSync('hub add CHANGELOG.md');
+}
+
+function postversion() {
+  const productionBranch = determineProductionBranch();
+  const milestone = `${versionPrefix}${packageVersion}`;
+
+  // publish branch
+  execSync(`hub push --follow-tags --set-upstream origin release/${packageVersion}`);
+  // create pull request
+  execSync(`hub pull-request --no-edit --b ${productionBranch} -m "${milestone}"`);
+}
+
+module.exports = { preversion, version, postversion };
